cdc1327de7508d7c1707101212d70b86f34f1afd
mardikarifqi <rifweb.android@gmail.com>
2019-09-04 00:32:11 +0700

    Considering performance vs latency
    Performance (how much request can be served at a time..)
    Latency (how long the dT for sending the data)

--- bootstrap/minifier.php
+++ bootstrap/minifier.php
@@ -32,32 +32,33 @@ class Minifier
 	{
 		$data = ob_get_clean();
 		$hash = substr(md5($data), 0, 10);
-		if (!$return) {
-			$path = "/" . __PUBLICDIR . "/cache/" . $hash . '.' . $file_ext;
-			if (file_exists(__ROOTDIR . $path)) return ("/cache/" . $hash . '.' . $file_ext);
+		$path = "/" . __PUBLICDIR . "/cache/" . $hash . '.' . $file_ext;
+		$exist = file_exists(__ROOTDIR . $path);
+		if (!$exist) {
+			switch ($file_ext) {
+				case "js":
+					$data = str_replace('type="text/javascript"', "", $data);
+					$data = str_replace("type='text/javascript'", "", $data);
+					$data = preg_replace("/\s*<(\h|)script(\h|)>\s*/", "", $data);
+					$data = preg_replace("/\s*<\/(\h|)script(\h|)>\s*/", "", $data);
+					$data = (new JS($data))->minify();
+					break;
+				case "css":
+					$data = str_replace('type="text/css"', "", $data);
+					$data = str_replace("type='text/css'", "", $data);
+					$data = preg_replace("/\s*<(\h|)style(\h|)>\s*/", "", $data);
+					$data = preg_replace("/\s*<\/(\h|)style(\h|)>\s*/", "", $data);
+					$data = (new CSS($data))->minify();
+					break;
+				default:
+					throw new PuzzleError("Only css and js are available to be minified");
+			}
+			IO::write($path, $data);
 		}
-		switch ($file_ext) {
-			case "js":
-				$data = str_replace('type="text/javascript"', "", $data);
-				$data = str_replace("type='text/javascript'", "", $data);
-				$data = preg_replace("/\s*<(\h|)script(\h|)>\s*/", "", $data);
-				$data = preg_replace("/\s*<\/(\h|)script(\h|)>\s*/", "", $data);
-				$data = (new JS($data))->minify();
-				break;
-			case "css":
-				$data = str_replace('type="text/css"', "", $data);
-				$data = str_replace("type='text/css'", "", $data);
-				$data = preg_replace("/\s*<(\h|)style(\h|)>\s*/", "", $data);
-				$data = preg_replace("/\s*<\/(\h|)style(\h|)>\s*/", "", $data);
-				$data = (new CSS($data))->minify();
-				break;
-			default:
-				throw new PuzzleError("Only css and js are available to be minified");
-		}
 		if (!$return) {
-			if (!file_exists(__ROOTDIR . $path)) IO::write($path, $data);
-			return ("/cache/" . $hash . '.' . $file_ext);
+			return "/cache/" . $hash . '.' . $file_ext;
 		} else {
+			if ($exist) $data = IO::read($path);
 			return $data;
 		}
 	}
